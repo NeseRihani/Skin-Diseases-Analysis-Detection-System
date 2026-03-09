52668c36f5b2372d7ecb9d7fd2bb9835770bca7f
NeseRihani <marahaltalaj123@gmail.com>
2026-03-09 23:04:55 +0300

    Initial commit: Skin Analysis with YOLO and N-Tier Architecture

---
+++ DataAccessLayer/Context/SkinAnalyseContext.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+	public class SkinAnalyseContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-95TG6R7\\SQLEXPRESS;initial catalog=SDD;integrated security=true;TrustServerCertificate=True;");
+		}
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Photo> Photos { get; set; }
+		public DbSet<Login> Logins { get; set; }
+		public DbSet<AnalyseResult> AnalyseResults { get; set; }
+		public DbSet<AIHistory> AIHistories { get; set; }
+	}
+}
---
+++ DataAccessLayer/EntityFrameWork/EfLoginDal.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Abstruct;
+using DataAccessLayer.Context;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFrameWork
+{
+	public class EfLoginDal : GenericRepository<Login>, ILoginDal
+
+	{
+        private readonly SkinAnalyseContext context;
+        public EfLoginDal(SkinAnalyseContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+        public string GetUserNameById(int id)
+        {
+            // Kullanıcı Tablosunda id'ye göre kullanıcı adını al
+            var userName = context.Users
+                .Where(u => u.userId == id)
+                .Select(u => u.userName)
+                .FirstOrDefault();
+
+            return userName;
+
+        }
+	}
+}
